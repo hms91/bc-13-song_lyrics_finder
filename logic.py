c6a9789ddd8d93d7ef5c6019f39194cb8566a5b9
unknown <hmphrmsn@gmail.com>
2016-12-08 02:27:40 +0300

    Logic Definitions

---
+++ logic.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import html5lib
+
+class FindLyrics(object):
+	def __init__(self):
+
+	def find(self, query_string):
+	# Returns a list of songs that match the criteria. 
+
+		return results
+
+	def view_by_id(self, song_id):
+	# View song lyrics based on it's id. Should be optimized by checking if there's a local copy before checking online. 
+
+		return results
+	
+	def save_song(self, song_id):
+	# Store song details and lyrics locally.
+
+		return results
+
+	def clear(self):
+	# Clear entire local song database. 
