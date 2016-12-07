e817d38cab581e4f7617dc3a9376054e5047fcae
unknown <hmphrmsn@gmail.com>
2016-12-08 02:44:33 +0300

    Find function trial run

--- logic.py
+++ logic.py
@@ -4,21 +4,36 @@ import html5lib
 
 class FindLyrics(object):
 	def __init__(self):
+		self.base_url = 'http://api.genius.com'
+		self.headers = {'Authorization': 'Bearer FFu14c1hWkoJWFtfoi7UNJk9o_vRPvflLHk8yt3IZa9Kj-4AmUR9UlEFpID9ITpH'}
+		self.search_url = self.base_url + "/search"
 
 	def find(self, query_string):
 	# Returns a list of songs that match the criteria. 
+		search_for = query_string.replace(' ','%20')
+		data = {'q': search_for}
+		try:
+			response = requests.get(self.search_url, data=data, headers=self.headers)
+		except:
+			raise requests.exceptions.ConnectionError('Error: Please check your Connection')
 
+		json = response.json()
+		results = json['response']['hits']
+		
 		return results
 
 	def view_by_id(self, song_id):
 	# View song lyrics based on it's id. Should be optimized by checking if there's a local copy before checking online. 
 
-		return results
-	
+		pass	
 	def save_song(self, song_id):
 	# Store song details and lyrics locally.
 
-		return results
-
+		pass
 	def clear(self):
 	# Clear entire local song database. 
+		pass
+
+song = FindLyrics()
+this_one = "Capsized"
+print song.find(this_one)
