7ce856978b5d485077ca5ce21230ba67ec6549c6
unknown <hmphrmsn@gmail.com>
2016-12-08 04:29:36 +0300

    Database Class, SongDetails Class, ArtistDetails Class

---
+++ model_base.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, UnicodeText
+
+Base = declarative_base()
+
+class DbObject(object):
+	def __init__(self):
+		self.engine = create_engine('sqlite:///:memory:')
+		Base.metadata.create_all(self.engine)
+
+	def start_session(self):
+		Session = sessionmaker(bind = self.engine)
+		self.session = Session()
+
+		return self
+
+	def clear_db_data(self):
+		Base.metadata.drop_all(self.engine)
+	
+		Base.metadata.create_all(self.engine)
+
+class SongDetails(Base):
+	__tablename__ = 'songdetails'
+	song_id = Column(Integer, primary_key=True)
+	artist_id = Column(Integer, ForeignKey('artistdetails.artist_id'))	
+	song_name = Column(String(40))
+	song_lyrics = Column(UnicodeText(64))
+
+class ArtistDetails(Base):
+	__tablename__ = 'artistdetails'
+	artist_id = Column(Integer, primary_key=True)
+	artist_name = Column(String(40))
